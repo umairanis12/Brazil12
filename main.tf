0954bffacd952639d9f54c306bd1ba260de52e6c
Umair Anis <umair.anis@gmail.com>
2025-07-09 16:13:21 +0100

    conflict add

--- main.tf
+++ main.tf
@@ -12,3 +12,7 @@ resource "azurerm_resource_group" "branch12" {
   name     = "branch12"
   location = "West Europe"
 }
+resource "azurerm_resource_group" "conflict12" {
+  name     = "conflict12"
+  location = "West Europe"
+}
